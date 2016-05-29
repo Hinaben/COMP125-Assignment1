8570be213f271bfa39fde4236a13adcbc6675c8c
Hinaben <henapatel21@hotmail.com>
2016-05-29 19:16:41 -0400

    Initial Commit

---
+++ Scripts/app.js
@@ -0,0 +1,35 @@
+/* Main JavaScript file */
+
+/*
+        Author:       Hina Patel (StuentID: 821021425)
+        File Name:    app.js
+        Description:  Java Script file for COMP 125- Assignment 1 web application. 
+*/
+
+// IIFE - Immediately Invoked Function Expression
+(function(){
+    "use strict";
+  
+  //define an array of HTML elements
+  var paragraphElements = [];
+  
+  paragraphElements[0]= document.getElementById("paragraphOne");  
+  paragraphElements[1]= document.getElementById("paragraphTwo");  
+  paragraphElements[2]= document.getElementById("paragraphThree");    
+  
+  //define your paragraphs array
+  var paragraphs = [];
+  
+  //
+  paragraphs[0] = "This page is my home page. The best is yet to come!"
+  paragraphs[1] = "This is my project page and project details will be added later."
+  paragraphs[2] = "My name is Hina Patel and my student number is 821021425."
+   
+    //check to see if paragraph one exists
+  for (var index = 0; index < paragraphElements.length; index++){
+    if(paragraphElements[index]){
+      paragraphElements[index].textContent = paragraphs[index];
+    }
+  }
+  
+})();
