096bbdeb97cea135a4ea92ea6920e42dd9109746
Hinaben <henapatel21@hotmail.com>
2016-06-03 19:39:45 -0400

    Changed java script variable names and fixed the form

--- Scripts/app.js
+++ Scripts/app.js
@@ -8,41 +8,40 @@
 
 // IIFE - Immediately Invoked Function Expression
 (function(){
-    "use strict";
-    
+    "use strict";    
  
   //define an array of HTML elements
   
-  var paragraphElements = [];
+  var documentElements = [];
   
-  paragraphElements[0]= document.getElementById("statement");  
-  paragraphElements[1]= document.getElementById("paragraphTwo");  
-  paragraphElements[2]= document.getElementById("paragraphThree");    
-  paragraphElements[3]= document.getElementById("work_address");
-  paragraphElements[4]= document.getElementById("pro_1_title");  
-  paragraphElements[5]= document.getElementById("pro_2_title");
-  paragraphElements[6]= document.getElementById("pro_3_title");
-  paragraphElements[7]= document.getElementById("bio_heading");
-  paragraphElements[8]= document.getElementById("name");
-  //define paragraphs array
-  var paragraphs = [];
+  documentElements[0]= document.getElementById("statement");  
+  documentElements[1]= document.getElementById("paragraphTwo");  
+  documentElements[2]= document.getElementById("paragraphThree");    
+  documentElements[3]= document.getElementById("work_address");
+  documentElements[4]= document.getElementById("pro_1_title");  
+  documentElements[5]= document.getElementById("pro_2_title");
+  documentElements[6]= document.getElementById("pro_3_title");
+  documentElements[7]= document.getElementById("bio_heading");
+  documentElements[8]= document.getElementById("name");
+  //define documentElementContent array
+  var documentElementContent = [];
     
-  paragraphs[0] = "<strong>To become a skilled and experienced software solution professional with extensive \
+  documentElementContent[0] = "<strong>To become a skilled and experienced software solution professional with extensive \
                     knowledge of all the areas of SDLC including requirements gathering, design, development \
                     and testing.<strong>";
-  paragraphs[1] = "This is my project page and project details will be added later."
-  paragraphs[2] = "My name is Hina Patel and my student number is 821021425."
-  paragraphs[3] = "<strong> CIBC </strong> - 750 - Lawerence Avenue W., Toronto"
-  paragraphs[4] = "Administrative Assistant"
-  paragraphs[5] = "Workforce Analyst"
-  paragraphs[6] = "Accounting Officer"
-  paragraphs[7] = "Mission Statement"
-  paragraphs[8] = "Hina Patel"
+  documentElementContent[1] = "This is my project page and project details will be added later."
+  documentElementContent[2] = "My name is Hina Patel and my student number is 821021425."
+  documentElementContent[3] = "<strong> CIBC </strong> - 750 - Lawerence Avenue W., Toronto"
+  documentElementContent[4] = "Administrative Assistant"
+  documentElementContent[5] = "Workforce Analyst"
+  documentElementContent[6] = "Accounting Officer"
+  documentElementContent[7] = "Mission Statement"
+  documentElementContent[8] = "Hina Patel"
   
-    //check to see if paragraph one exists
-  for (var index = 0; index < paragraphElements.length; index++){
-    if(paragraphElements[index]){
-      paragraphElements[index].innerHTML = paragraphs[index];
+  // check to see if elment exists
+  for (var index = 0; index < documentElements.length; index++){
+    if(documentElements[index]){
+      documentElements[index].innerHTML = documentElementContent[index];
     }
   }
   
